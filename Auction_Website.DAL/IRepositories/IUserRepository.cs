b8355d62c29438defc7a771a5c68308240b24d47
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 20:52:08 +0100

    User Repository method implementation and interface declaration

---
+++ Auction_Website.DAL/IRepositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Auction_Website.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.IRepositories
+{
+    public interface IUserRepository : IRepository<ApplicationUser>
+    {
+        Task<ApplicationUser> GetByEmailAsync(string email);
+    }
+}
