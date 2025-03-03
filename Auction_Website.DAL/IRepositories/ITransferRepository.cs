84cbf3e762ba08cf5427673ce57e196c613a1416
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 21:55:37 +0100

    Transfer repository and interface implementation

---
+++ Auction_Website.DAL/IRepositories/ITransferRepository.cs
@@ -0,0 +1,14 @@
+﻿using Auction_Website.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.IRepositories
+{
+    public interface ITransferRepository : IRepository<Transfer>
+    {
+        Task<IEnumerable<Transfer>> GetTransfersByUserIdAsync(int userId);
+    }
+}
---
+++ Auction_Website.DAL/Repositories/TransferRepository.cs
@@ -0,0 +1,29 @@
+﻿using Auction_Website.DAL.Entities;
+using Auction_Website.DAL.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.Repositories
+{
+    public class TransferRepository : Repository<Transfer>, ITransferRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransferRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transfer>> GetTransfersByUserIdAsync(int userId)
+        {
+            return await _context.Transfers
+                .Where(t => t.FromUserId == userId || t.ToUserId == userId)
+                .OrderByDescending(t => t.TransferDate)
+                .ToListAsync();
+        }
+    }
+}
