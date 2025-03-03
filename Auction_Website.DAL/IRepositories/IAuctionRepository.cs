896af51d8b3638ac4193b0d02e6d01c3193e1149
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 21:54:54 +0100

    Auction Repository and interface implementation

---
+++ Auction_Website.DAL/IRepositories/IAuctionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Auction_Website.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.IRepositories
+{
+    public interface IAuctionRepository : IRepository<Auction>
+    {
+        Task<IEnumerable<Auction>> GetActiveAuctionsAsync();
+        Task<IEnumerable<Auction>> GetAuctionsByUserIdAsync(int userId);
+    }
+}
---
+++ Auction_Website.DAL/Repositories/AuctionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Auction_Website.DAL.Entities;
+using Auction_Website.DAL.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.Repositories
+{
+    public class AuctionRepository : Repository<Auction>, IAuctionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuctionRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Auction>> GetActiveAuctionsAsync()
+        {
+            return await _context.Auctions
+                .Where(a => a.EndTime > System.DateTime.UtcNow && !a.IsClosed)
+                .OrderBy(a => a.EndTime)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Auction>> GetAuctionsByUserIdAsync(int userId)
+        {
+            return await _context.Auctions
+                .Where(a => a.CreatedByUserId == userId)
+                .ToListAsync();
+        }
+    }
+}
