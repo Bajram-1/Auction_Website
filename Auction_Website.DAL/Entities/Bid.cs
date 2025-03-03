f2772f5415da547cd67df4f7fb21d5923005cc92
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 21:02:05 +0100

    Added Bid Entity To Database

---
+++ Auction_Website.DAL/Entities/Bid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.Entities
+{
+    public class Bid
+    {
+        public int BidId { get; set; }
+        [Required]
+        public int AuctionId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime BidTime { get; set; } = DateTime.UtcNow;
+
+        public Auction Auction { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
