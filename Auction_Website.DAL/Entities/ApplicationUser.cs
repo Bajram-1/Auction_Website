6f580dc99390a44c57076501b1e110e7bc639d02
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 20:54:32 +0100

    Modified application user to inherit from system user

--- Auction_Website.DAL/Entities/ApplicationUser.cs
+++ Auction_Website.DAL/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,22 +9,13 @@ using System.Threading.Tasks;
 
 namespace Auction_Website.DAL.Entities
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
-        public int UserId { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 3)]
-        public string Username { get; set; }
-        [Required]
-        [MinLength(8)]
-        public string PasswordHash { get; set; }
-        [Required]
-        public string Email { get; set; }
         [DataType(DataType.Currency)]
         public decimal Wallet { get; set; } = 1000.00m;
-        public ICollection<Auction> AuctionsCreated { get; set; }
-        public ICollection<Bid> Bids { get; set; }
-        public ICollection<Transfer> TransfersSent { get; set; }
-        public ICollection<Transfer> TransfersReceived { get; set; }
+        //public ICollection<Auction> AuctionsCreated { get; set; }
+        //public ICollection<Bid> Bids { get; set; }
+        //public ICollection<Transfer> TransfersSent { get; set; }
+        //public ICollection<Transfer> TransfersReceived { get; set; }
     }
 }
