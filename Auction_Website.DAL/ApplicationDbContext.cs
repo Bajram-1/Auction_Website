5cc183fa283265a0aea50a5492a47e4ffc409865
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 21:58:00 +0100

    Created Appliation Db Context

---
+++ Auction_Website.DAL/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Auction_Website.DAL.Entities;
+
+namespace Auction_Website.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Transfer> Transfers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Auction>()
+                .HasMany(a => a.Bids)
+                .WithOne(b => b.Auction)
+                .HasForeignKey(b => b.AuctionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Bids)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.TransfersSent)
+                .WithOne(t => t.FromUser)
+                .HasForeignKey(t => t.FromUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.TransfersReceived)
+                .WithOne(t => t.ToUser)
+                .HasForeignKey(t => t.ToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
