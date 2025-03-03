deee2419ab5ad7448fb6b2537eaeb1abd392dec2
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-03 21:03:02 +0100

    Created Transfer Entity to database

---
+++ Auction_Website.DAL/Entities/Transfer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.Entities
+{
+    public class Transfer
+    {
+        public int TransferId { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        [Required]
+        public int FromUserId { get; set; }
+        [Required]
+        public int ToUserId { get; set; }
+        [Required]
+        public DateTime TransferDate { get; set; } = DateTime.UtcNow;
+        [StringLength(200)]
+        public string Reason { get; set; }
+        public ApplicationUser FromUser { get; set; }
+        public ApplicationUser ToUser { get; set; }
+    }
+}
