8093e5c2b18e2777a1bed635160e349a688715a3
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-04 11:19:25 +0100

    Used Fluent API to handle the validations of Bid Entity properties

---
+++ Auction_Website.DAL/DbConfig/BidConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Auction_Website.DAL.Entities;
+
+namespace Auction_Website.DAL.DbConfig
+{
+    public class BidConfiguration : IEntityTypeConfiguration<Bid>
+    {
+        public void Configure(EntityTypeBuilder<Bid> builder)
+        {
+            builder.ToTable("Bids");
+
+            builder.HasKey(b => b.BidId);
+
+            builder.Property(b => b.Amount)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(b => b.BidTime)
+                .IsRequired();
+
+            builder.HasOne(b => b.User)
+                .WithMany(u => u.Bids)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(b => b.Auction)
+                .WithMany(a => a.Bids)
+                .HasForeignKey(b => b.AuctionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
