f59290f40e0bc2f1a5733374433b9938b07bf28d
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-04 11:29:51 +0100

    Created unit of work to have a centralized unit for repositories to save changes

---
+++ Auction_Website.DAL/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction_Website.DAL.IRepositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; }
+        IAuctionRepository AuctionRepository { get; }
+        IBidRepository BidRepository { get; }
+        ITransferRepository TransferRepository { get; }
+
+        Task SaveChangesAsync();
+        Task<T> ExecuteTransactionAsync<T>(Func<Task<T>> func);
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        void Dispose();
+    }
+}
