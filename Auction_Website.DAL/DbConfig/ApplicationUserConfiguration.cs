2717b13357eb816daa3383d2c9d2b6712eda4269
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-04 11:11:23 +0100

    Used Fluent API to handle the validations of Application User Entity properties

---
+++ Auction_Website.DAL/DbConfig/ApplicationUserConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Auction_Website.DAL.Entities;
+
+namespace Auction_Website.DAL.DbConfig
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(u => u.Wallet)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.HasMany(u => u.AuctionsCreated)
+                .WithOne(a => a.CreatedByUser)
+                .HasForeignKey(a => a.CreatedByUserId);
+
+            builder.HasMany(u => u.Bids)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId);
+
+            builder.HasMany(u => u.TransfersSent)
+                .WithOne(t => t.FromUser)
+                .HasForeignKey(t => t.FromUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(u => u.TransfersReceived)
+                .WithOne(t => t.ToUser)
+                .HasForeignKey(t => t.ToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
