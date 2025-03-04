e152073e8e7825d2f08df0935e9a66ebd7b21b42
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-04 11:19:56 +0100

    Used Fluent API to handle the validations of Transfer Entity properties

---
+++ Auction_Website.DAL/DbConfig/TransferConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Auction_Website.DAL.Entities;
+
+namespace Auction_Website.DAL.DbConfig
+{
+    public class TransferConfiguration : IEntityTypeConfiguration<Transfer>
+    {
+        public void Configure(EntityTypeBuilder<Transfer> builder)
+        {
+            builder.ToTable("Transfers");
+
+            builder.HasKey(t => t.TransferId);
+
+            builder.Property(t => t.Amount)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(t => t.TransferDate)
+                .IsRequired();
+
+            builder.Property(t => t.Reason)
+                .HasMaxLength(200);
+
+            builder.HasOne(t => t.FromUser)
+                .WithMany(u => u.TransfersSent)
+                .HasForeignKey(t => t.FromUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.ToUser)
+                .WithMany(u => u.TransfersReceived)
+                .HasForeignKey(t => t.ToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
