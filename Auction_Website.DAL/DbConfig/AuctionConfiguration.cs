4ac3954bb4ec2e07ca5320e5e5703f31be936300
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-04 11:18:34 +0100

    Used Fluent API to handle the validations of Auction Entity properties

---
+++ Auction_Website.DAL/DbConfig/AuctionConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Auction_Website.DAL.Entities;
+
+namespace Auction_Website.DAL.DbConfig
+{
+    public class AuctionConfiguration : IEntityTypeConfiguration<Auction>
+    {
+        public void Configure(EntityTypeBuilder<Auction> builder)
+        {
+            builder.ToTable("Auctions");
+
+            builder.HasKey(a => a.AuctionId);
+
+            builder.Property(a => a.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(a => a.Description)
+                .IsRequired();
+
+            builder.Property(a => a.StartingPrice)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(a => a.StartTime)
+                .IsRequired();
+
+            builder.Property(a => a.EndTime)
+                .IsRequired();
+
+            builder.HasMany(a => a.Bids)
+                .WithOne(b => b.Auction)
+                .HasForeignKey(b => b.AuctionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
