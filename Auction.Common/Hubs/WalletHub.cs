b681ecdb3fc3fb9480d06cdde483d8aa4da9bcdf
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-05 23:36:28 +0100

    Using signal R to calculate transaction automatically between poster and bidder after an auction is closed if there is an offer

---
+++ Auction.Common/Hubs/WalletHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Auction_Website.UI.Hubs
+{
+    public class WalletHub : Hub
+    {
+        public async Task SendWalletUpdate(string userId, decimal newBalance)
+        {
+            await Clients.User(userId).SendAsync("ReceiveWalletUpdate", newBalance);
+        }
+
+        public async Task AuctionClosed(int auctionId)
+        {
+            await Clients.All.SendAsync("AuctionClosed", auctionId);
+        }
+    }
+}
