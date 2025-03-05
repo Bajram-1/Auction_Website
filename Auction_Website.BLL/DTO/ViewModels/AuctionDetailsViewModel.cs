4f4229a90c64370f0ec7ace197702e7ab756f6a2
Bajram Shehi <bajram.shehi98@gmail.com>
2025-03-05 12:38:20 +0100

    Auction Details View Model implementation

---
+++ Auction_Website.BLL/DTO/ViewModels/AuctionDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Auction_Website.BLL.DTO.ViewModels
+{
+    public class AuctionDetailsViewModel
+    {
+        public int AuctionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string SellerName { get; set; }
+        public decimal StartingPrice { get; set; }
+        public DateTime EndTime { get; set; }
+        public string CreatedByUserId { get; set; }
+        public bool IsClosed { get; set; }
+        public decimal CurrentHighestBid { get; set; }
+        public TimeSpan TimeRemaining => EndTime - DateTime.UtcNow;
+        public List<BidViewModel> Bids { get; set; } = new List<BidViewModel>();
+    }
+}
